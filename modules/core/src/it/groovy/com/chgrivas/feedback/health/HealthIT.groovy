b0c94f7e275d14445359bdcc0176614467298653
Chris Grivas <chgrivas7@gmail.com>
2018-06-11 01:32:44 +0300

    Add integration tests structure

---
+++ modules/core/src/it/groovy/com/chgrivas/feedback/health/HealthIT.groovy
@@ -0,0 +1,14 @@
+package com.chgrivas.feedback.health
+
+import spock.lang.Specification
+
+
+class HealthIT extends Specification {
+
+	def "App is up and running"() {
+		when:
+			def a = 1
+		then:
+			a == 2
+	}
+}
