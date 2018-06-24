93844cec4802b116301ae8658ef79994c268a0e0
Chris Grivas <chgrivas7@gmail.com>
2018-06-24 03:47:42 +0300

    Add memoized solution

--- modules/core/src/main/java/com/chgrivas/feedback/core/algorithms/generic/FibonacciSolver.java
+++ modules/core/src/main/java/com/chgrivas/feedback/core/algorithms/generic/FibonacciSolver.java
@@ -1,11 +1,30 @@
 package com.chgrivas.feedback.core.algorithms.generic;
 
+import java.math.BigInteger;
+import java.util.Map;
+
 public class FibonacciSolver {
 
-  int getFibonacciRecursively(int n) {
-    if (n == 0 || n == 1) {
-      return 1;
+  BigInteger getFibonacciRecursively(BigInteger n) {
+    if (BigInteger.ZERO.equals(n) || BigInteger.ONE.equals(n)) {
+      return BigInteger.ONE;
     }
-    return getFibonacciRecursively(n - 1) + getFibonacciRecursively(n - 2);
+    return getFibonacciRecursively(n.subtract(BigInteger.valueOf(1))).add(getFibonacciRecursively(n.subtract(BigInteger.valueOf(2))));
+  }
+
+  BigInteger getFibonacciMemoized(BigInteger n, Map<BigInteger, BigInteger> memo) {
+
+    if (memo.containsKey(n)) {
+      return memo.get(n);
+    }
+
+    BigInteger result;
+    if (BigInteger.ZERO.equals(n) || BigInteger.ONE.equals(n)) {
+      result = BigInteger.valueOf(1);
+    } else {
+      result = getFibonacciMemoized(n.subtract(BigInteger.valueOf(1)), memo).add(getFibonacciMemoized(n.subtract(BigInteger.valueOf(2)), memo));
+    }
+    memo.put(n, result);
+    return result;
   }
 }
--- modules/core/src/test/groovy/com/chgrivas/feedback/core/algorithms/generic/FibonacciSolverSpec.groovy
+++ modules/core/src/test/groovy/com/chgrivas/feedback/core/algorithms/generic/FibonacciSolverSpec.groovy
@@ -13,15 +13,27 @@ class FibonacciSolverSpec extends Specification {
     fibonacciSolver = new FibonacciSolver()
   }
 
-  def "recursive algorith works as expected"() {
+  def "recursive algorithm works as expected"() {
     when:
       Instant before = Instant.now()
-      def result = fibonacciSolver.getFibonacciRecursively(45)
+      def result = fibonacciSolver.getFibonacciRecursively(BigInteger.valueOf(20))
       Instant after = Instant.now()
     then:
       Duration timeElapsed = Duration.between(before, after);
-      println "Finished in ${timeElapsed.toMillis()} millis"
+      println "Algorithm finished in ${timeElapsed.toMillis()} millis with result ${result}"
     and:
-      result == 1836311903
+      result == 10946
+  }
+
+  def "memoized recursive algorithm works as expected"() {
+    when:
+      Instant before = Instant.now()
+      def result = fibonacciSolver.getFibonacciMemoized(BigInteger.valueOf(1000), new HashMap<>())
+      Instant after = Instant.now()
+    then:
+      Duration timeElapsed = Duration.between(before, after);
+      println "Memoized algorithm finished in ${timeElapsed.toMillis()} millis with result ${result}"
+    and:
+      result == 70330367711422815821835254877183549770181269836358732742604905087154537118196933579742249494562611733487750449241765991088186363265450223647106012053374121273867339111198139373125598767690091902245245323403501
   }
 }
