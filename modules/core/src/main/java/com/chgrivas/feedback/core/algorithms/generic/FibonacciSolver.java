c496960c43d4738cb59980bff33935e605e024a6
Chris Grivas <chgrivas7@gmail.com>
2018-06-24 04:11:05 +0300

    Add botom up approach

--- modules/core/src/main/java/com/chgrivas/feedback/core/algorithms/generic/FibonacciSolver.java
+++ modules/core/src/main/java/com/chgrivas/feedback/core/algorithms/generic/FibonacciSolver.java
@@ -1,18 +1,19 @@
 package com.chgrivas.feedback.core.algorithms.generic;
 
 import java.math.BigInteger;
+import java.util.HashMap;
 import java.util.Map;
 
 public class FibonacciSolver {
 
-  BigInteger getFibonacciRecursively(BigInteger n) {
+  public BigInteger getFibonacciRecursively(BigInteger n) {
     if (BigInteger.ZERO.equals(n) || BigInteger.ONE.equals(n)) {
       return BigInteger.ONE;
     }
     return getFibonacciRecursively(n.subtract(BigInteger.valueOf(1))).add(getFibonacciRecursively(n.subtract(BigInteger.valueOf(2))));
   }
 
-  BigInteger getFibonacciMemoized(BigInteger n, Map<BigInteger, BigInteger> memo) {
+  public BigInteger getFibonacciMemoized(BigInteger n, Map<BigInteger, BigInteger> memo) {
 
     if (memo.containsKey(n)) {
       return memo.get(n);
@@ -26,5 +27,27 @@ public class FibonacciSolver {
     }
     memo.put(n, result);
     return result;
+  }
+
+  public BigInteger getFibonacciBottomUp(BigInteger n) {
+
+    Map<BigInteger, BigInteger> memo = new HashMap<>();
+
+    memo.put(BigInteger.valueOf(0), BigInteger.ONE);
+    memo.put(BigInteger.valueOf(1), BigInteger.ONE);
+
+    if (memo.containsKey(n)) {
+      return memo.get(n);
+    }
+
+    BigInteger i = BigInteger.valueOf(2);
+    while (i.compareTo(n) <= 0) {
+      BigInteger before1 = memo.get(i.subtract(BigInteger.valueOf(1)));
+      BigInteger before2 = memo.get(i.subtract(BigInteger.valueOf(2)));
+      memo.put(i, before1.add(before2));
+      i = i.add(BigInteger.valueOf(1));
+    }
+
+    return memo.get(n);
   }
 }
--- modules/core/src/test/groovy/com/chgrivas/feedback/core/algorithms/generic/FibonacciSolverSpec.groovy
+++ modules/core/src/test/groovy/com/chgrivas/feedback/core/algorithms/generic/FibonacciSolverSpec.groovy
@@ -19,7 +19,7 @@ class FibonacciSolverSpec extends Specification {
       def result = fibonacciSolver.getFibonacciRecursively(BigInteger.valueOf(20))
       Instant after = Instant.now()
     then:
-      Duration timeElapsed = Duration.between(before, after);
+      Duration timeElapsed = Duration.between(before, after)
       println "Algorithm finished in ${timeElapsed.toMillis()} millis with result ${result}"
     and:
       result == 10946
@@ -31,8 +31,20 @@ class FibonacciSolverSpec extends Specification {
       def result = fibonacciSolver.getFibonacciMemoized(BigInteger.valueOf(1000), new HashMap<>())
       Instant after = Instant.now()
     then:
-      Duration timeElapsed = Duration.between(before, after);
+      Duration timeElapsed = Duration.between(before, after)
       println "Memoized algorithm finished in ${timeElapsed.toMillis()} millis with result ${result}"
+    and:
+      result == 70330367711422815821835254877183549770181269836358732742604905087154537118196933579742249494562611733487750449241765991088186363265450223647106012053374121273867339111198139373125598767690091902245245323403501
+  }
+
+  def "bottom up algorithm works as expected"() {
+    when:
+      Instant before = Instant.now()
+      def result = fibonacciSolver.getFibonacciBottomUp(BigInteger.valueOf(1000))
+      Instant after = Instant.now()
+    then:
+      Duration timeElapsed = Duration.between(before, after)
+      println "Bottom up algorithm finished in ${timeElapsed.toMillis()} millis with result ${result}"
     and:
       result == 70330367711422815821835254877183549770181269836358732742604905087154537118196933579742249494562611733487750449241765991088186363265450223647106012053374121273867339111198139373125598767690091902245245323403501
   }
