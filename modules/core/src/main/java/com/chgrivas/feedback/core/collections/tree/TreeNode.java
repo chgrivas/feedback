5a3c323e5349131edaf23bbf0f4248dd507f3876
Chris Grivas <chgrivas7@gmail.com>
2018-06-25 01:02:27 +0300

    Add tree node and in order traversal

---
+++ modules/core/src/main/java/com/chgrivas/feedback/core/collections/tree/TreeNode.java
@@ -0,0 +1,44 @@
+package com.chgrivas.feedback.core.collections.tree;
+
+public class TreeNode<T> {
+  private T data;
+
+  public T getData() {
+    return data;
+  }
+
+  public void setData(T data) {
+    this.data = data;
+  }
+
+  public TreeNode getLeft() {
+    return left;
+  }
+
+  public void setLeft(TreeNode left) {
+    this.left = left;
+  }
+
+  public TreeNode getRight() {
+    return right;
+  }
+
+  public void setRight(TreeNode right) {
+    this.right = right;
+  }
+
+  private TreeNode left;
+  private TreeNode right;
+
+  public static void inOrderTraversal(TreeNode node) {
+    if (node != null) {
+      inOrderTraversal(node.left);
+      visit(node);
+      inOrderTraversal(node.right);
+    }
+  }
+
+  public static void visit(TreeNode node) {
+    System.out.println(node.data);
+  }
+}
